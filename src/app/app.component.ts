8788c7717cf3d07c979363afae2c2c3afc18d6e8
Neven Mekkawy <NMekkawy@BlueCloudCorp.Com>
2022-04-23 14:42:59 +0200

    ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'AnalysisChart';
+
+  ngOnDestroy(): void {
+    localStorage.removeItem('selectedCountry');
+    localStorage.removeItem('selectedCamp');
+    localStorage.removeItem('selectedSchool');
+    localStorage.removeItem('chartList');
+  }
 }
