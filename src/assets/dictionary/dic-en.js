c21305998ebc8aee5af5044c4f2f28ff380ad949
Neven Mekkawy <NMekkawy@BlueCloudCorp.Com>
2022-04-24 02:29:45 +0200

    dic

--- src/assets/dictionary/dic-en.js
+++ src/assets/dictionary/dic-en.js
@@ -1,2 +1,12 @@
 var dic_en = {
+    shared: {
+        'country': 'Country',
+        'camp': 'Camp',
+        'school': 'School',
+        'lessons': 'Lessons',
+        'month': 'Month'
+    },
+    chartPointDetails: {
+        'chartPointDetails': 'Chart Point Details',
+    }
 }
